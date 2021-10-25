be1c2ee3e3458f071173ab67178bbc01a31894ef
Rodrigo Lins <rlinsdev@gmail.com>
2021-10-25 08:17:24 -0300

    Router and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-export const Navbar = () => {
+export const Navbar = ({ darkTheme, setDarkTheme }) => {
   return (
     <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center boder-b dark:border-gray-700 border-gray-200">
         <div className="flex justify-between itens-center space-x-5 w-screen"></div>
-      
+        <Link to="/">
+          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark: text-gray-900">
+            KD 🔎
+          </p>
+        </Link>
+        <button type="button" onClick={() => setDarkTheme(!darkTheme)}>
+          {darkTheme? 'Light': 'Dark'}
+        </button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import  { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './global.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Router>
+  <App />
+</Router>, 
+document.getElementById('root')
+);
