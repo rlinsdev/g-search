b3ca33a68ad050b698c8f1cb0386679d65838766
Rodrigo Lins <rlinsdev@gmail.com>
2021-10-26 08:40:19 -0300

    result context - repidAPI

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,27 @@
+import React,  {createContext, useContext, useState } from 'React';
+
+const ResultContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const ResultContextProvider = ({children}) => {
+  const [result, setResults] = useState ([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (type) => {
+    setIsLoading(true);
+
+    const response = await fetch(`${baseUrl}${type}`,{
+      method:'GET',
+      headers: {
+        'x-user-agent': 'desktop',
+        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+        'x-rapidapi-key': 'c8ca60d4c7msh7adc58209160d39p1708a5jsn2287cd086652'
+      }
+    });
+
+    const data = await response.json();
+    setResults(data);
+    setIsLoading(false);
+  }
+}
